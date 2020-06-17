e338168780e2cc880206c9645f0105b2cdf3d593
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-06-17 14:06:44 -0400

    fixed form for meals

--- src/components/form/FormCard.js
+++ src/components/form/FormCard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import "./FormCard.css"
 
 class FormCard extends Component {
     render() {
@@ -7,36 +9,30 @@ class FormCard extends Component {
         <div className="card">
           <div className="card-content">
             <h3>Meal Request</h3>
+            <p>Please fill out our form to request a meal</p>
             <Form>
   <Form.Group controlId="exampleForm.ControlInput1">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="name@example.com" />
+    <Form.Label>Caregiver Name</Form.Label>
+    <Form.Control type="text" placeholder="Full Name" />
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlInput2">
+    <Form.Label>Child Age</Form.Label>
+    <Form.Control type="number" placeholder="Age" />
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlSelect1">
-    <Form.Label>Example select</Form.Label>
+    <Form.Label>Neighborhood</Form.Label>
     <Form.Control as="select">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
+      <option>Downtown Charleston</option>
+      <option>Switzer Center</option>
+      <option>West Side</option>
+      <option>Kanawha City</option>
+      <option>South Park Village</option>
     </Form.Control>
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlSelect2">
-    <Form.Label>Example multiple select</Form.Label>
-    <Form.Control as="select" multiple>
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </Form.Control>
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlTextarea1">
-    <Form.Label>Example textarea</Form.Label>
-    <Form.Control as="textarea" rows="3" />
   </Form.Group>
 </Form>
+<Button variant="primary" size="sm">
+      Submit
+    </Button>
           </div>
         </div>
       );
