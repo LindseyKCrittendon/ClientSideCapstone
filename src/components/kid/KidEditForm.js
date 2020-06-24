225b1e1c1c11ccddd52b9fd35600942cd632a2af
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-06-24 14:02:01 -0400

    dropdown now prepopulating correct neighborhood name

--- src/components/kid/KidEditForm.js
+++ src/components/kid/KidEditForm.js
@@ -7,59 +7,60 @@ import NeighborhoodManager from "../../modules/NeighborhoodManager"
 // TODO:: FIX ISSUES WITH DROPDOWN SELECT.  IF USER DOES NOT CLICK => RETURNS NULL.  REPOPULATES CORRECT VALUE IN EDITING BEHIND THE SCENES BUT NOT VISUALLY FOR THE USER
 
 class KidEditForm extends Component {
-    //set the initial state
-    state = {
-        caregiver: "",
-        age: "",
-        neighborhoodId: "",
-        neighborhoods: [],
-        served: false,
-      loadingStatus: true,
+  //set the initial state
+  state = {
+    caregiver: "",
+    age: "",
+    neighborhoodId: "",
+    neighborhoods: [],
+    served: false,
+    selected: true,
+    loadingStatus: true,
+  };
+
+  handleFieldChange = evt => {
+    const stateToChange = {}
+    stateToChange[evt.target.id] = evt.target.value
+    this.setState(stateToChange)
+  }
+
+  updateExistingKid = evt => {
+    evt.preventDefault()
+    this.setState({ loadingStatus: true });
+    const editedKid = {
+      id: this.props.match.params.kidId,
+      caregiver: this.state.caregiver,
+      age: this.state.age,
+      neighborhoodId: parseInt(this.state.neighborhoodId),
+      served: false
     };
 
-    handleFieldChange = evt => {
-      const stateToChange = {}
-      stateToChange[evt.target.id] = evt.target.value
-      this.setState(stateToChange)
-    }
-
-    updateExistingKid = evt => {
-      evt.preventDefault()
-      this.setState({ loadingStatus: true });
-      const editedKid = {
-        id: this.props.match.params.kidId,
-        caregiver: this.state.caregiver,
-        age: this.state.age,
-        neighborhoodId: parseInt(this.state.neighborhoodId),
-        served: false
-      };
-
-      KidManager.update(editedKid)
+    KidManager.update(editedKid)
       .then(() => this.props.history.push("/kids"))
-    }
+  }
 
-    componentDidMount() {
-      KidManager.get(this.props.match.params.kidId)
+  componentDidMount() {
+    KidManager.get(this.props.match.params.kidId)
       .then(kid => {
-          this.setState({
-            caregiver: kid.caregiver,
-            age: kid.age,
-            neighborhoodId: kid.neighborhoodId,
-            served: false,
-            loadingStatus: false,
-          });
+        this.setState({
+          caregiver: kid.caregiver,
+          age: kid.age,
+          neighborhoodId: kid.neighborhoodId,
+          served: false,
+          loadingStatus: false,
+        });
       });
-      NeighborhoodManager.getAll()
+    NeighborhoodManager.getAll()
       .then((neighborhoods) => {
-          this.setState({
-              neighborhoods: neighborhoods
-          })
+        this.setState({
+          neighborhoods: neighborhoods
+        })
       })
-    }
+  }
 
-    render() {
-      return (
-        <>
+  render() {
+    return (
+      <>
         <form>
           <fieldset>
             <div className="formgrid">
@@ -83,15 +84,19 @@ class KidEditForm extends Component {
               />
               <label htmlFor="age">Age</label>
               <Form.Group>
-                                <Form.Label>Neighborhood</Form.Label>
-                                <Form.Control as="select"  onChange={this.handleFieldChange}
-                                id="neighborhoodId">
-                                {/* try onchange where select is */}
-                                {this.state.neighborhoods.map(neighborhood =>
-                                <option value={neighborhood.id}>{neighborhood.name}</option>
-                                 )}
-                                </Form.Control>
-                            </Form.Group>
+                <Form.Label>Neighborhood</Form.Label>
+                <Form.Control as="select" onChange={this.handleFieldChange}
+                  id="neighborhoodId">
+                  {/* try onchange where select is */}
+                  {this.state.neighborhoods.map(neighborhood =>
+                    neighborhood.id === this.state.neighborhoodId ?
+                      <option value={neighborhood.id} selected="true">{neighborhood.name}</option>
+                      :
+
+                      <option value={neighborhood.id}>{neighborhood.name}</option>
+                  )}
+                </Form.Control>
+              </Form.Group>
             </div>
             <div className="alignRight">
               <button
@@ -102,9 +107,9 @@ class KidEditForm extends Component {
             </div>
           </fieldset>
         </form>
-        </>
-      );
-    }
+      </>
+    );
+  }
 }
 
 export default KidEditForm
